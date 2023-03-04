192fce08d9552640d81d059535844f24bab8fd4b
ramdhangar65 <34364274+ramdhangar65@users.noreply.github.com>
2023-03-04 19:02:19 +0530

    Update cicd.groovy
    test the war file on same jenkins server

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -8,3 +8,6 @@ def newMaven(){
 def newDeployment(jobname,ip,context){
   sh "scp /home/ubuntu/.jenkins/workspace/${jobname}/target/java-jenkins-0.0.1-SNAPSHOT.war ubuntu@${ip}:/home/ubuntu/deployment/${context}.war"
 }
+def runDeployWarOnSameServer(jobname){
+  sh "java -jar /home/ubuntu/.jenkins/workspace/${jobname}/target/java-jenkins-0.0.1-SNAPSHOT.war"
+}
